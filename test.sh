02c2c0d3e4946d4771deb0da4c0083be3919f02d
zirus92 <sebasatlas92@gmail.com>
2026-05-08 20:18:46 -0600

    actualizacion

--- test.sh
+++
@@ -1,6 +0,0 @@
-export PS1='$(echo hello)'
-echo "With NO backslash:"
-bash --rcfile /dev/null -i -c 'echo ""'
-export PS1='\$(echo hello)'
-echo "With backslash:"
-bash --rcfile /dev/null -i -c 'echo ""'
